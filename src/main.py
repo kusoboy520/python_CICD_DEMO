cac86905ffbf253c02d718353d0ff62269a914b4
JamesLine <kusoboy5202@gmail.com>
2024-03-16 19:24:15 +0800

    fix: fix python type

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
+from typing import Union
 
-def calculate_area_square(length: int | float) -> int | float:
+
+def calculate_area_square(length: Union[int ,float]) -> Union[int ,float]:
     if not isinstance(length, (int, float)) or length <= 0:
         raise TypeError("Length must be a positive non-zero number")
     return length * length
